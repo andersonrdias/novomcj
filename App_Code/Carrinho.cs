04500d1acebf5fdf9cf8036568c7d859f2db3516
Anderson R Dias <andersonrdias@gmail.com>
2014-03-30 23:31:25 -0300

    Edição na pagina produtos

---
+++ App_Code/Carrinho.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Linq;
+/// <summary>
+/// TecnoSite carrinho de compras
+/// </summary>
+public class Carrinho
+{
+    private List<CarrinhoItem> _Itens = new List<CarrinhoItem>();
+    public List<CarrinhoItem> Itens { get { return _Itens; } }
+    public decimal ValorTotal { get { return _Itens.Sum(p => p.Preco); } }
+    public string LocalEntrega = "";
+
+    public void AdicionarItem(int produtoID, string descricao, decimal preco)
+    {
+        _Itens.Add( new CarrinhoItem { ProdutoID = produtoID, Descricao = descricao, Preco = preco } );
+    }
+    public void Limpar()
+    {
+        _Itens.Clear();
+    }
+    public void RemoverItem(int index)
+    {
+        _Itens.RemoveAt(index);
+    }
+    public void DefinirUF_Entrega (string UF){
+        LocalEntrega = UF;
+    }
+
+}
---
+++ App_Code/CarrinhoItem.cs
@@ -0,0 +1,10 @@
+﻿/// <summary>
+/// TecnoSite  item do carrinho
+/// </summary>
+public class CarrinhoItem
+{
+   public int ProdutoID { get; set; }
+   public string Descricao { get; set; }
+   public int Quantidade { get; set; }
+   public decimal Preco { get; set; }
+}
