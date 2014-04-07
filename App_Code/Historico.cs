8521cf8c810ec5d81b5314670f249c1cb642202d
Anderson R Dias <andersonrdias@gmail.com>
2014-04-07 16:12:25 -0300

    Histórico

---
+++ App_Code/Historico.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Linq;
+/// <summary>
+/// 
+/// </summary>
+public class Historico
+{
+    private List<HistoricoItem> _Itens = new List<HistoricoItem>();
+    public List<HistoricoItem> Itens { get { return _Itens; } }
+    public decimal ValorTotal { get { return _Itens.Sum(p => p.Preco); } }
+    public string LocalEntrega = "";
+
+    public void AdicionarItem(int produtoID, string descricao, decimal preco)
+    {
+        _Itens.Add( new HistoricoItem { ProdutoID = produtoID, Descricao = descricao, Preco = preco } );
+    }
+    public void Limpar()
+    {
+        _Itens.Clear();
+    }
+    public void RemoverItem(int index)
+    {
+        _Itens.RemoveAt(index);
+    }
+    public void DefinirUF_Entrega (string UF){
+        LocalEntrega = UF;
+    }
+
+}
---
+++ App_Code/HistoricoItem.cs
@@ -0,0 +1,10 @@
+﻿/// <summary>
+/// 
+/// </summary>
+public class HistoricoItem
+{
+   public int ProdutoID { get; set; }
+   public string Descricao { get; set; }
+   public int Quantidade { get; set; }
+   public decimal Preco { get; set; }
+}
